394be42ff16fd545314f2070f625fce08818fc13
Ryan Cloherty <cloherty.ryan@gmail.com>
2020-01-17 16:15:22 -0500

    Adjust program

--- JavaInterop/Program.cs
+++ JavaInterop/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ice;
+using System;
+using SimpleWifi;
 
 namespace JavaInterop
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            try
+            {
+                using (Ice.Communicator communicator = Ice.Util.initialize(ref args))
+                {
+                    var wifiApiAdapter =
+                        communicator.createObjectAdapterWithEndpoints("SimpleWiFiAdapter", "default -h localhost -p 10001");
+                    wifiApiAdapter.add(new WiFiApiI(), Ice.Util.stringToIdentity("SimpleWiFi"));
+                    wifiApiAdapter.activate();
+                    communicator.waitForShutdown();
+                }
+            }
+            catch (System.Exception e)
+            {
+                Console.Error.WriteLine(e);
+                return 1;
+            }
+            return 0;
+        }
+    }
+    public class WiFiApiI : WiFiApi.ApiHandleDisp_
+    {
+        private Wifi wifi = new Wifi();
+        public override void disconnectAll(Current current = null)
+        {
+            wifi.Disconnect();
         }
     }
 }
