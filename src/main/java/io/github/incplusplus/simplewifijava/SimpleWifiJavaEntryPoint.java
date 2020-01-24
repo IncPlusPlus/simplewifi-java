b08fa4dc847e8471d6e9c8b4eb0596c122178a8a
Ryan Cloherty <cloherty.ryan@gmail.com>
2020-01-24 18:22:59 -0500

    Added SimpleWifiJavaEntryPoint

---
+++ src/main/java/io/github/incplusplus/simplewifijava/SimpleWifiJavaEntryPoint.java
@@ -0,0 +1,11 @@
+package io.github.incplusplus.simplewifijava;
+
+/**
+ * This class is empty and serves only as a usable reference to a class
+ * contained within the simplewifi-java JAR file. To see an example of
+ * this in action, see the <a href="https://github.com/IncPlusPlus/bigtoolbox-network">bigtoolbox-network</a>
+ * Windows module. It uses this class to be able to extract the JavaInterop.exe file from this JAR
+ * and run it.
+ */
+public class SimpleWifiJavaEntryPoint {
+}
